277ddd89ef454189abc6b2d57d7145a62c693f3b
Dima Syzonenko <sizonenko.d18@gmail.com>
2023-03-01 20:55:35 +0200

    text mistake in CallToAction component fixed

--- src/components/CallToAction/CallToAction.jsx
+++ src/components/CallToAction/CallToAction.jsx
@@ -14,8 +14,7 @@ const CallToAction = () => {
                     <h4 className={styles["call-to-action__title"]}>Check your child’s <br /> math level</h4>
                     <div className={styles["call-to-action__grades"]}>Grades 1-8</div>
                     <p>Take a knowledge evaluation test to determine your child's math level and <span>receive a
-                        learning plan</span>
-                        to thrive in math</p>
+                        learning plan</span> to thrive in math</p>
                     <ul className={styles["call-to-action__list"]}>
                         <li><span>A 1-on-1 live lesson with math teacher</span></li>
                         <li><span>Your kid math level analysis with strong and weak sides outline</span></li>
